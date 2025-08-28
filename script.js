1d2a12fe1d9395be6d89bb8a481b27984a872f0d
Nandini-871 <nandini898087@gmail.com>
2025-08-28 13:33:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('a[href^="#"]').forEach
+(anchor =>{
+    anchor.addEventListener('click',function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        })
+    })
+})
+//nav scroll//
+window.addEventListener('scroll', ()=>{
+    const navbar = document.querySelector('.navbar');
+    window.scrollY > 50 ?
+   navbar.style.background ='rgba(10,10,10,0.98)' :
+    navbar.style.background ='rgba(10,10,10,0.95)';
+});
+
